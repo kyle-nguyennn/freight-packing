fa1c04a27e0b3f4207659cdb091035817dc40854
Kyle Nguyen <nghia002@outlook.com>
2021-05-03 01:52:04 +0800

    clean: add comments

--- src/solvers/blf-heuristic.ts
+++ src/solvers/blf-heuristic.ts
@@ -4,7 +4,8 @@ export interface SpaceDivisable {
     splitSpace(container: TripleShape, obj: TripleShape): TripleShape[];
 }
 export class BLFHeuristic implements SpaceDivisable{
-    // in this class empty space will be split by occupied object into top, front, and right
+    // Bottom Left Front first - place the object in the bottom left front corner
+    // Empty space will be split by the occupied object into top, front, and right
     splitSpace(container: TripleShape, obj: TripleShape): TripleShape[] {
         const result: TripleShape[] = [];
         if (container.height > obj.height)
